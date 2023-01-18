f7c166fb4e1921fb8d694d2a5535cd3505257aac
Daimonke <Daimonas98@gmail.com>
2023-01-18 22:26:16 +0200

    UPD: shared Head in all pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,18 @@
+import Head from 'next/head'
 import Navbar from './Navbar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
+            <Head>
+                <title>Rasa-Us Art</title>
+                <meta name="description" content="Rasa-Us Art" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Navbar />
             <main>{children}</main>
         </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,8 @@ import Head from 'next/head'
 
 export default function Home() {
     return (
-        <>
-            <Head>
-                <title>Rasa-Us Art</title>
-                <meta name="description" content="Rasa-Us Art" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main>
-                <h1 className="text-red-600">Hello world!!</h1>
-            </main>
-        </>
+        <main>
+            <h1 className="text-red-600">Hello world!!</h1>
+        </main>
     )
 }

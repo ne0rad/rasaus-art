da6c1524ce30cd3f5908b4773ae13426c9a111fd
ne0rad <usackasr@gmail.com>
2023-01-19 20:00:29 +0000

    FIX: typo

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
       <>
         <title>Rasos Meno Terapija</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="description" content="RasaUs Art Teraphy website" />
+        <meta name="description" content="RasaUs Art Therapy website" />
         <link rel="icon" href="/favicon.ico" />
       </>
     )

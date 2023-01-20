cfc6f7e199cc15e818dc498c42be39fbbdcab7d2
ne0rad <usackasr@gmail.com>
2023-01-20 17:10:27 +0000

    UPD: title

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
     return (
       <>
-        <title>Rasos Meno Terapija</title>
+        <title>RasaUs Art/Therapy</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="description" content="RasaUs Art Therapy website" />
         <link rel="icon" href="/favicon.ico" />
